3c294a644cbd170bbf6c7378ace89c251c8ec926
caloto <43333627+caloto@users.noreply.github.com>
2020-06-24 17:55:09 +0200

    Bad-spelling
    Bad-spelling is more correct than just spelling in the sentence.

--- R/cnaintexp.r
+++ R/cnaintexp.r
@@ -89,6 +89,7 @@ CNAintEXP <- function(genes = c(),
     if(tumor %in% tumors) {
       print(paste("Analyzing ", tumor, "...", sep=""))
     }else{
+      print(paste("The introduced cohort: ", tumor, " was not found among the available TCGA cohorts. Bad-spelling is a common mistake, please check it or select any of the available ones.", sep=""))
       next
     }
     final.col.names <- c("SYMBOL", "P.VAL", "CNA.FC", "PATIENTS.PERCENTAGE","COND", "TEST", "TUMOR", "PAT.ID")
